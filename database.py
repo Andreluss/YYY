983b571c16ad3d1897009af7de01439e70619184
Andrzej Jabłoński <64368904+Andreluss@users.noreply.github.com>
2024-06-20 22:58:30 +0200

    update info

--- database.py
+++ database.py
@@ -7,5 +7,5 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # used in get_db function
+Base = declarative_base() # dziwny niby-typ, z którego dziedziczą modele tabel
