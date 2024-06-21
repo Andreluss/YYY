74c20efa1627a0f3098b3ab717ef4c22d80aeb56
Andrzej Jabłoński <64368904+Andreluss@users.noreply.github.com>
2024-06-21 22:15:18 +0200

    api update

--- models.py
+++ models.py
@@ -1,5 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+import sqlalchemy.types as types
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from database import Base
+import json
+
 
 # ---------------- Database Table Models ***Edit as needed*** ---------------- #
 class Books(Base):
@@ -11,13 +16,15 @@ class Books(Base):
     description = Column(String)
     rating = Column(Integer)
 
+
 class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String) #, unique=True)
+    username = Column(String)  # , unique=True)
     email = Column(String)
     password = Column(String)
+
 
 class Reviews(Base):
     __tablename__ = "reviews"
@@ -27,3 +34,38 @@ class Reviews(Base):
     user_id = Column(Integer)
     review = Column(String)
     rating = Column(Integer)
+
+
+class Tags(Base):
+    __tablename__ = "tags"
+    id = Column(Integer, primary_key=True, index=True)
+    tag = Column(String)
+
+
+image_tag_table = Table(
+    "link_tags",
+    Base.metadata,
+    Column("image_id", Integer, ForeignKey("images.id")),
+    Column("tag_id", Integer, ForeignKey("tags.id"))
+)
+
+
+class JsonWrapper(types.TypeDecorator):
+    impl = types.Unicode
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        return json.dumps(value)
+
+    def process_result_value(self, value, dialect):
+        return json.loads(value)
+
+class Images(Base):
+    __tablename__ = "images"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    user_id = Column(Integer)
+    description = Column(String)
+    rectangles = Column(JsonWrapper)
+    tags = relationship(Tags, secondary=image_tag_table)
--- server.py
+++ server.py
@@ -16,19 +16,18 @@ from pydantic import BaseModel
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from typing import List
+from fastapi.encoders import jsonable_encoder
+import random
+import json
 
 # --------------- fast api config [DON'T TOUCH] --------------- #
 app = FastAPI()
 
 # ------ Enable CORS ------ #
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:8081",
-]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -36,7 +35,8 @@ app.add_middleware(
 
 # ------------------ database config [DON'T TOUCH] ------------------ #
 # this will create the db and tables if they don't exist (all table models from models.py)
-models.Base.metadata.create_all(bind=engine) 
+models.Base.metadata.create_all(bind=engine)
+
 
 def get_db():
     try:
@@ -45,10 +45,10 @@ def get_db():
     finally:
         db.close()
 
-# -------------------------------------------------------------------------------- #
-# -------------------------------------------------------------------------------- #
-# -------------------------------------------------------------------------------- #
 
+# -------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------- #
 
 
 # ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -78,36 +78,28 @@ def get_db():
 # ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 
 
-
-
-
-
-
 # -------------------------------------------------------------------------------- #
 # ------------------ API Models ***Edit as needed*** ------------------ #
 # -------------------------------------------------------------------------------- #
 
-class Book(BaseModel): # to jest api model, nie model z bazy danych (patrz models.py) 
-    title: str = Field(min_length=1) # ten przydaje sie do walidacji api requestów
+class Book(BaseModel):  # to jest api model, nie model z bazy danych (patrz models.py)
+    title: str = Field(min_length=1)  # ten przydaje sie do walidacji api requestów
     author: str = Field(min_length=1, max_length=100)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=-1, lt=101)
-    
+
+
 # kolejny api model - do walidacji requestów, nie musi być coupled z modelem z bazy danych
 class Review(BaseModel):
     review: str = Field(min_length=1, max_length=1000)
     rating: int = Field(gt=-1, lt=101)
+
 
 class User(BaseModel):
     username: str = Field(min_length=1, max_length=100)
     # email with regex validation
     email: str = Field(min_length=1, max_length=100, pattern="[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+")
     password: str = Field(min_length=1, max_length=100)
-
-
-
-
-
 
 # -------------------------------------------------------------------------------- #
 # ------------------ API Endpoints ***Edit as needed*** -------------------------- #
@@ -120,6 +112,7 @@ class User(BaseModel):
 def get_all_books(db: Session = Depends(get_db)):
     return db.query(models.Books).all()
 
+
 @app.post("/")
 def create_book(book: Book, db: Session = Depends(get_db)):
     # tworzymy obiekt modelu z bazy danych
@@ -128,15 +121,16 @@ def create_book(book: Book, db: Session = Depends(get_db)):
     db.add(book_model)
     # i zapisać zmiany(!)
     db.commit()
-    
+
     return book
+
 
 @app.put("/{book_id}")
 def update_book(book_id: int, book: Book, db: Session = Depends(get_db)):
     book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
     if book_model is None:
         raise HTTPException(status_code=404, detail=f"Book with id {book_id} not found")
-    
+
     book_model.title = book.title
     book_model.author = book.author
     book_model.description = book.description
@@ -144,19 +138,19 @@ def update_book(book_id: int, book: Book, db: Session = Depends(get_db)):
     db.commit()
     return book
 
+
 @app.delete("/{book_id}")
 def delete_book(book_id: int, db: Session = Depends(get_db)):
     book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
     if book_model is None:
         raise HTTPException(status_code=404, detail=f"Book with id {book_id} not found")
-    
+
     db.delete(book_model)
     db.commit()
     return f"Book with id {book_id} deleted"
+
+
 # _________ _________ _________ _________ _________ _________ _________ _________ #
-
-
-
 
 
 # _________ _________ _________ _________ _________ _________ _________ _________ #
@@ -165,38 +159,43 @@ def delete_book(book_id: int, db: Session = Depends(get_db)):
 def get_book_reviews(book_id: int, db: Session = Depends(get_db)):
     return db.query(models.Reviews).filter(models.Reviews.book_id == book_id).all()
 
-@app.post("/book_reviews/{book_id}/{user_id}") # common practice is to use path params for (resource) ids 
-def create_book_review(book_id: int, user_id: int, review: Review, db: Session = Depends(get_db)): 
+
+@app.post("/book_reviews/{book_id}/{user_id}")  # common practice is to use path params for (resource) ids
+def create_book_review(book_id: int, user_id: int, review: Review, db: Session = Depends(get_db)):
     # ensure the user and book exist
     user = db.query(models.Users).filter(models.Users.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail=f"User with id {user_id} not found")
-    
+
     book = db.query(models.Books).filter(models.Books.id == book_id).first()
     if book is None:
         raise HTTPException(status_code=404, detail=f"Book with id {book_id} not found")
-    
+
     review_model = models.Reviews(book_id=book_id, user_id=user_id, review=review.review, rating=review.rating)
     db.add(review_model)
     db.commit()
     return review
+
 
 @app.get("/book_reviews/{book_id}/average_rating")
 def get_book_average_rating(book_id: int, db: Session = Depends(get_db)):
     reviews = db.query(models.Reviews).filter(models.Reviews.book_id == book_id).all()
     if len(reviews) == 0:
         return {"average_rating": 0}
-    
+
     total_rating = sum([review.rating for review in reviews])
     return {"average_rating": total_rating / len(reviews)}
+
 
 @app.get("/user_reviews/{user_id}")
 def get_user_reviews(user_id: int, db: Session = Depends(get_db)):
     return db.query(models.Reviews).filter(models.Reviews.user_id == user_id).all()
 
+
 @app.get("/users")
 def get_users(db: Session = Depends(get_db)):
     return db.query(models.Users).all()
+
 
 # endpoint for creating new user
 @app.post("/users")
@@ -205,6 +204,7 @@ def create_user(user: User, db: Session = Depends(get_db)):
     db.add(user_model)
     db.commit()
     return user
+
 
 # seed the database with some initial data
 @app.get("/seed-database")
@@ -239,14 +239,9 @@ def seed_database(db: Session = Depends(get_db)):
 
     db.commit()
     return "Database seeded"
+
+
 # _________ _________ _________ _________ _________ _________ _________ _________ #
-
-
-
-
-
-
-
 
 
 # _________ _________ _________ _________ _________ _________ _________ _________ #
@@ -257,17 +252,11 @@ async def async_sleep_1s():
     await asyncio.sleep(1)  # simulate I/O bound task
     return {"message": "Hello, World!"}
 
+
 # see websocket_endpoint below ....
-    
+
 
 # _________ _________ _________ _________ _________ _________ _________ _________ #
-
-
-
-
-
-
-
 
 
 # _________ _________ _________ _________ _________ _________ _________ _________ #
@@ -277,23 +266,23 @@ async def async_sleep_1s():
 class ConnectionManager:
     def __init__(self):
         self.active_connections = []
-        
+
     async def connect(self, websocket):
         await websocket.accept()
         self.active_connections.append(websocket)
-        
+
     def disconnect(self, websocket):
         self.active_connections.remove(websocket)
-        
+
     async def send_personal_message(self, message, websocket):
         await websocket.send_text(message)
-        
+
     async def broadcast(self, message):
         for connection in self.active_connections:
             await connection.send_text(message)
 
-manager = ConnectionManager()
 
+manager = ConnectionManager()
 
 # ----------------- API Websocket Endpoints ***Edit as needed*** ----------------- #
 # -------------------------------------------------------------------------------- #
@@ -340,9 +329,12 @@ html2 = """
     </body>
 </html>
 """
+
+
 @app.get("/ws-chat-demo")
 async def get():
     return HTMLResponse(html2)
+
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
@@ -355,3 +347,151 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast(f"Client {client_id} left the chat")
+
+
+async def random_sleep():
+    await asyncio.sleep(30)
+
+
+def random_rectangle():
+    return {
+        'x': random.randint(1, 100),
+        'y': random.randint(1, 100),
+        'width': random.randint(1, 200),
+        'height': random.randint(1, 200),
+        'color': random.choice(['white', 'yellow'])
+    }
+
+
+def generate_rectangles():
+    return [random_rectangle() for _ in range(random.randint(1, 10))]
+
+
+async def generate_random_image(db):
+    random_image = Image(title="Pozdro", user_id=2, description="600", rectangles=generate_rectangles(), tags=["xd"])
+    await add_image(random_image, db)
+
+    return db.query(models.Images).all()[-1].id
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket, db: Session = Depends(get_db)):
+    await manager.connect(websocket)
+    try:
+        while True:
+            await random_sleep()
+            text_id = await generate_random_image(db)
+            text_to_send = '[' + str(text_id) + ']'
+            print(text_to_send)
+
+            await manager.broadcast(text_to_send)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+
+# ---------------------------------------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------------------------------------- #
+
+
+class Rectangle(BaseModel):
+    x: int = Field(gt=-1)
+    y: int = Field(gt=-1)
+    width: int = Field(gt=0)
+    height: int = Field(gt=0)
+    color: str = Field(min_length=1)
+
+
+class Image(BaseModel):
+    title: str = Field(min_length=1)
+    user_id: int = Field(gt=-1)
+    description: str = Field(min_length=0)
+    rectangles: List[Rectangle] = Field(min_items=0)
+    tags: List[str] = Field(min_items=0)
+
+
+# 1. Daj obrazek o konkretnym id
+@app.get('/images/{picture_id}')
+async def get_image_by_id_endpoint(picture_id: int, db: Session = Depends(get_db)):
+    if random.randrange(100) < 25:
+        raise HTTPException(status_code=500, detail='dupa')
+
+    if random.randrange(100) < 25:
+        await asyncio.sleep(10)
+
+    res = db.query(models.Images).filter(models.Images.id == picture_id).first()
+    if res is None:
+        return HTTPException(status_code=404, detail='Not found')
+    return res
+
+# 2. Daj listę dostępnych id
+
+# 3. Dodaj obrazek (z nowym id z DB)
+from typing import Dict, Any
+
+@app.post('/images')
+async def add_image(image: Image, db: Session = Depends(get_db)):
+    tag_models = []
+    for tag in image.tags:
+        tag_model = db.query(models.Tags).filter(models.Tags.tag == tag).first()
+        if tag_model is None:
+            tag_model = models.Tags(tag=tag)
+            db.add(tag_model)
+            tag_models.append(tag_model)
+    image_model = models.Images(title=image.title, user_id=image.user_id,
+        description=image.description, tags=tag_models, rectangles=jsonable_encoder(image.rectangles))
+    db.add(image_model)
+    db.commit()
+
+
+@app.get('/images')
+async def get_images(db: Session = Depends(get_db)):
+    images = db.query(models.Images).all()
+    return [image.id for image in images]
+
+
+# 4. Podaj listę dostępnych tagów
+@app.get('/tags')
+async def get_tags(db: Session = Depends(get_db)):
+    tags = db.query(models.Tags).all()
+    return [tag.tag for tag in tags]
+
+
+# 5. Usuń obrazek
+@app.delete('/images/{image_id}')
+async def delete_image(image_id: int, db: Session = Depends(get_db)):
+    to_delete = db.query(models.Images).filter(models.Images.id == image_id).first()
+    if to_delete is None:
+        return HTTPException(status_code=404)
+    db.delete(to_delete)
+    db.commit()
+
+# 6. Zmodyfikuj obrazek
+@app.put('/iamges/{image_id}')
+async def update_image(image_id: int):
+    pass
+
+
+# ---------------------
+# Filter by tag
+# Fetch single image by id
+# Fetch all images id (filter by tags)
+# [request]:
+# tags: [ tag1, tag2, tag3]
+# tags: [] -> all images
+# [Response]:
+# [1, 2, 3, 5, 8, 13, 21] (image_id)
+#
+#
+# Daj obrazek
+# {
+# 1. title
+# 2. description
+# 3. lista rectangli: [{x: 0, y: 0, width: 100, height: 100, color: "color"},
+#                      {x: 0, y: 0, width: 100, height: 100, color="#fff"}]
+# 4. author_id: int (user_id) # --> users table
+# 5. tags: [tag1, tag2, tag3] # --> tags table
+# }
+#
+#
+#
+
